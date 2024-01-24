9030772bef9a9d0ccd3025170aed47777baeceaa
Teste teste <nathmoonz@hotmail.com>
2024-01-24 09:21:20 -0300

    Adicionado Spring Security

---
+++ src/main/java/com/generation/blogpessoal/security/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.generation.blogpessoal.security;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.blogpessoal.model.Usuario;
+import com.generation.blogpessoal.repository.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	// injeção de dependenvias para conseguir consultar no banco
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		Optional<Usuario> usuario = usuarioRepository.findByUsuario(username);
+		if(usuario.isPresent())
+			return new UserDetailsImpl(usuario.get());
+		else
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+	}
+
+}
